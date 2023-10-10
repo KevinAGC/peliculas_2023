b05c0c51a02b547f8366f5de41215fa8d500c827
Kevin Garcia <kevin2001garciacarrasco@gmail.com>
2023-10-10 15:02:27 -0600

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas_2023/providers/movies_provider.dart';
 import 'package:peliculas_2023/screens/screens.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          lazy: false,
+        )
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,8 +32,8 @@ class MyApp extends StatelessWidget {
       title: 'Peliculas',
       initialRoute: 'home',
       routes: {
-        'home': (_) => const HomeScreen(),
-        'details': (_) => const DetailsScreen(),
+        'home': (_) => HomeScreen(),
+        'details': (_) => DetailsScreen(),
       },
     );
   }
